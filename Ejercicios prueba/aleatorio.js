8288a4eddad6b294b78df1a151c7b153bb3081bc
Damaris <damarisrmarscal@gmail.com>
2020-05-19 17:18:42 -0500

    Actualizaciones

---
+++ Ejercicios prueba/aleatorio.js
@@ -0,0 +1,15 @@
+function inicio() {
+  console.log("Hola, soy JS");
+  let numeroAle         atorioi`CF = Math.floor(Math.random() * 10);
+  // Math.floor(n) 5.2 = 5     5.9 = 5
+  // Math.random() = 10  =  0........9
+  console.log(numeroAleatorio)
+
+  let intento = prompt("Adivina el número")
+  console.log(intento)
+}
+
+if (intento != numeroAleatorio)
+  alert("Le atinaste")
+else
+  alert("Nope, no es ese número")
